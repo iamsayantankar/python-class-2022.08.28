74f5435174214d7f254d6965ca4539b36ef72b8e
sayan <sayantankar01@gmail.com>
2022-08-28 22:54:37 +0530

    Initial commit

---
+++ demo_01.py
@@ -0,0 +1,41 @@
+# Swaping Process
+
+
+# Process 01
+a=90
+b=77
+
+c=b
+b=a
+a=c
+print(a,b)
+
+
+# Process 02
+a=90
+b=77
+
+a = a+b
+b = a-b
+a=a-b
+print(a,b)
+
+
+# Process 03
+a=90
+b=77
+
+a,b = b,a
+print(a,b)
+
+
+
+
+# Doubt
+a=8
+b=9
+c=10
+
+a,b,c = b,c,a
+
+print(a,b,c)
